196edc7dae852783072e4847b58ef212a085b226
Jonathan Engel <joow.engeel@gmail.com>
2023-04-08 11:25:38 -0300

    starting to implement Board and Piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -9,5 +9,17 @@ package boardgame;
  * @author Jow
  */
 public class Piece {
-    
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
 }

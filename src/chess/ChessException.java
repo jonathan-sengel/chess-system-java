135c26963519797037f83b719d3f91b3314cd7c6
Jonathan Engel <joow.engeel@gmail.com>
2023-04-08 15:45:13 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chess;
+
+/**
+ *
+ * @author Jow
+ */
+public class ChessException extends RuntimeException {
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
